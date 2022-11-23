41b8662ebaafe54cbeda6cfdf19475c2564ff782
Saif eddine El hanoune <sel-hano@e1r10p15.1337.ma>
2022-11-23 06:18:15 +0100

    SHELL

---
+++ FirstDay.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "hey i'm trying up to learn bash scripting for the first time on my part time"
+
+sleep 1
+
+echo "i've sleept for 1 second please excuse me another 10s"
+
+sleep 10
+
+echo "okey good bye i've to be executed"
